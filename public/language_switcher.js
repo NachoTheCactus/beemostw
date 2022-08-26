e6339595a03d7ccb895fdf02ad419d0f3eb8a83e
DarkMaaaster <teoconan16@yahoo.es>
2022-08-26 18:55:12 +0300

    Start of project

---
+++ public/language_switcher.js
@@ -0,0 +1,26 @@
+
+
+loadLanguageContent("en");
+
+function loadLanguageContent(lang_code){
+    fetch('languages/' + lang_code + '.json')
+    .then(res => res.json())
+    .then((res) => {
+        var keys = Object.keys(res);
+        var values = Object.values(res);
+
+        var content =  Object.assign.apply({}, keys.map( (v, i) => ( {[v]: values[i]} ) ) );
+        for(id in content){
+            // let selectedElement = document.getElementById(id);
+            // if(selectedElement){
+            //     selectedElement.innerHTML = content[id];
+            // }
+            let selectedElements = document.getElementsByClassName(id);
+            console.log(selectedElements);
+            for(let i = 0; i < selectedElements.length; i++){
+                selectedElements[i].innerHTML = content[id];
+            }
+        }
+    });
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["public/*.{html,js}"],
+  theme: {
+    colors:{
+      'bmo': {
+        y: '#EBB142',
+        b: '#772804',
+      }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
